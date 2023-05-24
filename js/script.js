fca6218b05474f3b8b7ce5815140ce15f81bc6c7
cookierx01 <134132778+cookierx01@users.noreply.github.com>
2023-05-24 22:33:11 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,15 @@
+function hitungLuas() {
+    var sisiLuas = document.getElementById("sisi-luas").value;
+
+    document.getElementById(hitung-Luas).addEventListener("click", hitung);
+    document.getElementById(output-luas).innerHTML = <span>sisiLuas</span>;
+    console.log(sisiLuas);
+}
+
+function hitungKeliling() {
+    var sisiKeliling = document.getElementById("sisi-keliling").value;
+
+    document.getElementById(hitung-sisiKeliling).addEventListener("click", hitung);
+    document.getElementById(output-keliling).innerHTML = <span>sisiKeliling</span>;
+    console.log(sisiKeliling);
+}
